fed8764c6d549101a227c079ec6885dcf7ca19c7
HAL-42 <hal_42@zju.edu.cn>
2023-03-28 10:54:02 +0800

    实验更多的L2 CI损失权重组合。

---
+++ configs/clip_cam/gcam,调CI/l2,fb等权,8到64k乘2/cfg.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Author  : Xiaobo Yang
+@Contact : hal_42@zju.edu.cn
+@Time    : 2023/3/10 12:16
+@File    : cfg.py
+@Software: PyCharm
+@Desc    : 
+"""
+from alchemy_cat.py_tools import Cfg2Tune, Param2Tune
+
+cfg = config = Cfg2Tune('configs/patterns/seg_metrics/cls_m_IoU,pra.py',
+                        cfgs_update_at_parser=('configs/clip_cam/gcam,调CI/base.py',))
+
+cfg.rslt_dir = ...
+
+# * 设置l2损失。
+cfg.loss.loss_items.cam_lb.ini.loss_type = 'l2'
+
+# * 设置l2损失的权重。
+cfg.loss.loss_items.cam_lb.weights = Param2Tune([(8000, 8000), (16000, 16000), (32000, 32000), (64000, 64000)])
+
+# * 对l2损失，设置val时的bg power为为2和4。
+cfg.val.cfg.eval.seed.bg_methods = [{'method': 'pow', 'pow': p} for p in (2, 4)]
