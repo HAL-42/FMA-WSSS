5572134612ee2917d1d0cb5aa45e3fc4a59ef6f8
HAL-42 <hal_42@zju.edu.cn>
2023-04-13 22:37:10 +0800

    结合子迭代算法，重现此前对比学习实验。

---
+++ configs/clip_cam/调cls/cl_loss/重现/1子迭/cfg.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Author  : Xiaobo Yang
+@Contact : hal_42@zju.edu.cn
+@Time    : 2023/3/23 23:20
+@File    : cfg.py
+@Software: PyCharm
+@Desc    : 
+"""
+from alchemy_cat.py_tools import Config
+
+cfg = config = Config('configs/clip_cam/调cls/cl_loss/cfg.py')
