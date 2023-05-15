8d6973383bc46ccb13136decd10d27f02b75d87b
HAL-42 <hal_42@zju.edu.cn>
2023-05-15 10:33:43 +0800

    尝试 1）用更大的模型训练离线伪真值。2）用更优初始化训练离线伪真值。

---
+++ configs/clip_cam/离线伪真,CI/l1/·125初/ps自cl_loss,5100,s真/cfg.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Author  : Xiaobo Yang
+@Contact : hal_42@zju.edu.cn
+@Time    : 2023/4/13 23:19
+@File    : base.py
+@Software: PyCharm
+@Desc    : 
+"""
+from alchemy_cat.py_tools import Config
+
+cfg = config = Config('configs/clip_cam/离线伪真,CI/l1/base.py',
+                      'configs/clip_cam/_patches/ref/coop_ctx-·125初/s真,M=16.py')
+
+# * 使用cl_loss，5100 val时的伪真值作为监督。
+cfg.dt.train.ini.ps_mask_dir = 'experiment/clip_cam/调cls/cl_loss/infer/5100/aff2次,at_score,ce_npp_crf,' \
+                               'mask/seed/best/mask'
