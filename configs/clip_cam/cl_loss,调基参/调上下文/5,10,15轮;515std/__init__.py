5f432a99ea773aebacbe906ebc59c909d4e14dae
HAL-42 <hal_42@zju.edu.cn>
2023-06-02 22:53:58 +0800

    实验cl loss，修改迭代次数、随机、std，但lr终止到0。

---
+++ configs/clip_cam/cl_loss,调基参/调上下文/5,10,15轮;515std/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Author  : Xiaobo Yang
+@Contact : hal_42@zju.edu.cn
+@Time    : 2023/5/3 18:01
+@File    : __init__.py.py
+@Software: PyCharm
+@Desc    : 
+"""
