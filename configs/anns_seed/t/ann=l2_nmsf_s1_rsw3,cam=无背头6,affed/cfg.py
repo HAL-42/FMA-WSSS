3c4d8fa58b2e13748254deea359902e1590a7861
HAL-42 <hal_42@zju.edu.cn>
2023-06-08 12:00:39 +0800

    增加trian、train aug上的sam自动分割。

---
+++ configs/anns_seed/t/ann=l2_nmsf_s1_rsw3,cam=无背头6,affed/cfg.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Author  : Xiaobo Yang
+@Contact : hal_42@zju.edu.cn
+@Time    : 2023/5/25 16:05
+@File    : cfg.py
+@Software: PyCharm
+@Desc    : 
+"""
+from alchemy_cat.py_tools import Config
+
+cfg = config = Config('configs/anns_seed/base.py')
+
+cfg.rslt_dir = ...
+
+cfg.dt.ini.split = 'train'
+
+# * 配置CAM路径。
+cfg.cam.dir = 'experiment/clip_cam/离线伪真,CI/l1/·125初/ps自cl_loss,5100,csc/bg无类名,头名,s机,M=16/infer/final,train/' \
+              'aff2次,at_cam,att1次,·5掩阈,ce_npp/cam_affed'
+
+# * 配置替补种子点路径。
+cfg.seed.dir = 'experiment/clip_cam/离线伪真,CI/l1/·125初/ps自cl_loss,5100,csc/bg无类名,头名,s机,M=16/infer/final,train/' \
+               'aff2次,at_cam,att1次,·5掩阈,ce_npp/seed/best/mask'
+
+# * 配置SAM标注路径。
+cfg.sam_anns.dir = 'experiment/sam_auto_seg/vh,t/pattern_key=l2_nmsf_s1_rsw3/anns'
