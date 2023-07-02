29c8d1f22ffa3c9be1ba2e38179df9f46f3b9dea
HAL-42 <hal_42@zju.edu.cn>
2023-07-02 18:35:29 +0800

    增加一批可视化与种子点生成实验。

--- src/tasks/标签处理/voc上色.py
+++ src/tasks/标签处理/voc上色.py
@@ -8,20 +8,18 @@
 @Software: PyCharm
 @Desc    : 
 """
-import os
-import os.path as osp
 import argparse
 import multiprocessing as mp
+import os
+import os.path as osp
+import sys
 
 import numpy as np
+from PIL import Image
 from alchemy_cat.contrib.evaluation.semantics_segmentation import eval_preds
-from tqdm import tqdm
-from cv2 import cv2
-
-from alchemy_cat.acplot import RGB2BGR
 from alchemy_cat.contrib.voc import label_map2color_map
-
-import sys
+from alchemy_cat.data.plugins import arr2PIL
+from tqdm import tqdm
 
 from libs.data import VOCAug2
 
@@ -32,9 +30,9 @@ def 上色_label_file(label_file: str):
     if not label_file.endswith('.png'):
         return
 
-    label = cv2.imread(osp.join(args.source, label_file), cv2.IMREAD_GRAYSCALE)
-    color_label = RGB2BGR(label_map2color_map(label))
-    cv2.imwrite(osp.join(args.target, label_file), color_label)
+    label = np.array(Image.open(osp.join(args.source, label_file)))
+    color_label = label_map2color_map(label).astype(np.uint8)
+    arr2PIL(color_label, order='RGB').save(osp.join(args.target, label_file))
 
 
 def ignore2bg(lb: np.ndarray):
@@ -44,14 +42,20 @@ def ignore2bg(lb: np.ndarray):
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-e', '--eval', type=int, default=0)
+parser.add_argument('-m', '--mp', type=int, default=1)
 parser.add_argument('-s', '--source', type=str)
 parser.add_argument('-t', '--target', type=str)
 args = parser.parse_args()
 
 os.makedirs(args.target, exist_ok=True)
 
-with mp.Pool(int(mp.cpu_count() * 0.8)) as p:
-    for _ in tqdm(p.imap_unordered(上色_label_file, os.listdir(args.source), chunksize=10),
+if args.mp:
+    with mp.Pool(int(mp.cpu_count() * 0.8)) as p:
+        for _ in tqdm(p.imap_unordered(上色_label_file, os.listdir(args.source), chunksize=10),
+                      dynamic_ncols=True, desc="处理", unit="张"):
+            pass
+else:
+    for _ in tqdm(map(上色_label_file, os.listdir(args.source)),
                   dynamic_ncols=True, desc="处理", unit="张"):
         pass
 
